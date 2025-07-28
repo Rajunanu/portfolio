3d99b60add30c506f8a43f2ebeaf08bed1052cf4
Narasimha Raju <vallepunarasimha0@gmail.com>
2025-07-28 12:18:33 +0530

    home page completed

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 function toggleTheme() {
     var element = document.body;
+    var cardElement = document.getElementById("IdCard");
     element.classList.toggle("darkTheme");
+    cardElement.classList.toggle("changeCard");
 }
