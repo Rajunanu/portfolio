8a6861bc071f4a7925a27bf9db7ebc7c167d2b2c
Narasimha Raju <vallepunarasimha0@gmail.com>
2025-08-03 03:28:08 +0530

    created phoneview and sideview for portfolio

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function toggleTheme() {
     element.classList.toggle("darkTheme");
     cardElement.classList.toggle("changeCard");
 }
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
